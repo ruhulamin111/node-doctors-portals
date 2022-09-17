b9f03d540c6791edcde8be4dd93761154290c455
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-17 17:45:50 +0600

    start doctors portals server

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello doctors portals')
+})
+
+app.listen(port, () => {
+    console.log(`doctors portals server running on ${port}`)
+})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zjrcntk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        console.log('doctors portals database connected');
+
+        await client.connect()
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
