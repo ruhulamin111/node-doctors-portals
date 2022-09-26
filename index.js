9cf29ec53e41b7439f3a7a86f886616a6aa971e9
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-26 23:02:22 +0600

    data passed for post

--- index.js
+++ index.js
@@ -8,6 +8,39 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zjrcntk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const servicesCollection = client.db('doctorsPortals').collection('services')
+        const bookingsCollection = client.db('doctorsPortals').collection('bookings')
+
+        app.get('/services', async (req, res) => {
+            const result = await servicesCollection.find({}).toArray()
+            res.send(result)
+        })
+
+        app.post('/bookings', async (req, res) => {
+            console.log(req.body);
+            const result = await bookingsCollection.insertOne(req.body);
+            res.send(result)
+            console.log(req.body);
+        })
+
+
+        console.log('doctors portals database connected');
+
+    } catch (error) {
+        console.log(error);
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
 app.get('/', (req, res) => {
     res.send('Hello doctors portals')
 })
@@ -15,26 +48,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`doctors portals server running on ${port}`)
 })
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zjrcntk.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-async function run() {
-    try {
-        console.log('doctors portals database connected');
-        await client.connect()
-        const servicesCollection = client.db('doctorsPortals').collection('services')
-
-        app.get('/services', async (req, res) => {
-            const result = await servicesCollection.find({}).toArray()
-            res.send(result)
-        })
-
-
-
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-run().catch(console.dir)
