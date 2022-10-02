61ce8ae68975ea05bb8c6be7d34e127366457285
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-03 01:07:56 +0600

    payment method create paymentintent

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 app.use(cors())
 app.use(express.json())
@@ -45,6 +46,18 @@ async function run() {
                 return res.status(403).send({ message: 'forbidden access' })
             }
         }
+
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const input = req.body;
+            const price = input.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card'],
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        })
 
         app.get('/doctors', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await doctorCollection.find().toArray()
